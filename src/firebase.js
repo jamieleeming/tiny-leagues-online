658e48e0bea942a621514a9799c0b62c78fd0e8d
jleuta <jamie.leeming@hotmail.co.uk>
2025-01-22 19:18:56 -0500

    security update

--- src/firebase.js
+++ src/firebase.js
@@ -3,17 +3,14 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your Firebase configuration (you'll get this from Firebase Console)
 const firebaseConfig = {
-    apiKey: "AIzaSyDnx42LC7G76r4Ht9hCV0jUOuBMzj67KiI",
-    authDomain: "tiny-leagues.firebaseapp.com",
-    projectId: "tiny-leagues",
-    storageBucket: "tiny-leagues.firebasestorage.app",
-    messagingSenderId: "274468686318",
-    appId: "1:274468686318:web:5b5e5266daaac9a5d3e6ff",
-    measurementId: "G-Q51EB4C0MY"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db }; 
+export const db = getFirestore(app); 
